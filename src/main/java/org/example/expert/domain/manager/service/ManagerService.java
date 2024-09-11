895f2df9c5ae72002d8ca8c749bf9c2db72e7414
Robbie <Robbie@email.com>
2024-09-11 17:20:07 +0900

    2-4 유닛테스트(3) todo.getUser() == null || 추가

--- src/main/java/org/example/expert/domain/manager/service/ManagerService.java
+++ src/main/java/org/example/expert/domain/manager/service/ManagerService.java
@@ -36,7 +36,7 @@ public class ManagerService {
         Todo todo = todoRepository.findById(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
 
-        if (!ObjectUtils.nullSafeEquals(user.getId(), todo.getUser().getId())) {
+        if (todo.getUser() == null || !ObjectUtils.nullSafeEquals(user.getId(), todo.getUser().getId())) {
             throw new InvalidRequestException("담당자를 등록하려고 하는 유저가 일정을 만든 유저가 유효하지 않습니다.");
         }
 
